2c908a0af8db8588eed8ee6b517aebcf78afc7d3
royalturd <samridhh.rudra@hotmail.com>
2022-09-25 22:14:02 +0530

    Migrate from  SQLite to PostgreSQL
    Signed-off-by: royalturd <samridhh.rudra@hotmail.com>

--- Expensum/Expensum/settings.py
+++ Expensum/Expensum/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -73,13 +74,23 @@ WSGI_APPLICATION = 'Expensum.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': os.environ.get('DATABASE_NAME'),
+       'USER': os.environ.get('DATABASE_USER'),
+       'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+       'HOST': os.environ.get('DATABASE_HOST'),
+       'PORT': os.environ.get('DATABASE_PORT'),
+   }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
