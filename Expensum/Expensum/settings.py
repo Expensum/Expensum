2b65a7d1d53c67cbd23fe07d07d13c6a484f345c
royalturd <samridhh.rudra@hotmail.com>
2023-04-27 15:19:26 +0530

    nuke postgres for good
    Signed-off-by: royalturd <samridhh.rudra@hotmail.com>

--- Expensum/Expensum/settings.py
+++ Expensum/Expensum/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'expenses'
 ]
 
 MIDDLEWARE = [
@@ -74,23 +75,23 @@ WSGI_APPLICATION = 'Expensum.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': os.environ.get('DATABASE_NAME'),
-       'USER': os.environ.get('DATABASE_USER'),
-       'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-       'HOST': os.environ.get('DATABASE_HOST'),
-       'PORT': os.environ.get('DATABASE_PORT'),
-   }
-}
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+     }
+ }
+
+#DATABASES = {
+#   'default': {
+#       'ENGINE': 'django.db.backends.postgresql',
+#       'NAME': os.environ.get('DATABASE_NAME'),
+#       'USER': os.environ.get('DATABASE_USER'),
+#       'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+#        'HOST': os.environ.get('DATABASE_HOST'),
+#        'PORT': os.environ.get('DATABASE_PORT'),
+#   }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
